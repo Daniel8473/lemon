ba439902b3a0183dd73143b39237bd63110027b3
Daniel8473 <Daniel_8473@hotmail.com>
2021-01-17 06:55:15 -0800

    -

--- lemon/HomeView.swift
+++ lemon/HomeView.swift
@@ -60,9 +60,6 @@ struct HomeView: View {
         
                 }
             }
-            NavigationLink(destination: QueueView()) {
-                Text("Login")
-            }
         }
     }
     
